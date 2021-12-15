5b3b217fdb39ed67777c7530457d3f21f5654820
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2021-12-15 20:10:25 +0530

    added bottom nav bar , included binding (#1)
    * added bottom nav bar , included binding
    
    * Extracted String Resources
    
    * added dimens, added string and color values instead of plain text

---
+++ app/src/main/java/org/openlake/sampoorna/ui/fragments/TrackingFragment.kt
@@ -0,0 +1,33 @@
+package org.openlake.sampoorna.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.transition.MaterialFadeThrough
+import org.openlake.sampoorna.R
+import org.openlake.sampoorna.databinding.FragmentTrackingBinding
+
+
+class TrackingFragment : Fragment(R.layout.fragment_tracking) {
+    //enabling data binding
+    private var _binding: FragmentTrackingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        exitTransition = MaterialFadeThrough()
+        enterTransition = MaterialFadeThrough()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentTrackingBinding.inflate(inflater,container,false)
+        val view = binding.root
+        return view
+    }
+}
