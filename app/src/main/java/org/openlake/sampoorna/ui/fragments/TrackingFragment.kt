06a2bdd1b7893490868d54a0a5b87af056dc7601
Aastha Chauhan <aastha.chauhan01@gmail.com>
2021-12-25 16:54:16 +0530

    Added UI for Tracker

--- app/src/main/java/org/openlake/sampoorna/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/org/openlake/sampoorna/ui/fragments/TrackingFragment.kt
@@ -1,33 +1,11 @@
-package org.openlake.sampoorna.ui.fragments
+package com.example.myperiodtracker
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.google.android.material.transition.MaterialFadeThrough
-import org.openlake.sampoorna.R
-import org.openlake.sampoorna.databinding.FragmentTrackingBinding
 
-
-class TrackingFragment : Fragment(R.layout.fragment_tracking) {
-    //enabling data binding
-    private var _binding: FragmentTrackingBinding? = null
-    private val binding get() = _binding!!
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        exitTransition = MaterialFadeThrough()
-        enterTransition = MaterialFadeThrough()
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        // Inflate the layout for this fragment
-        _binding = FragmentTrackingBinding.inflate(inflater,container,false)
-        val view = binding.root
-        return view
+        setContentView(R.layout.activity_main)
     }
 }
