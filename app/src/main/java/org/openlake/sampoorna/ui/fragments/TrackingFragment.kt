bb43e3a9c72a1b7d2fd91a695c8638a72eeaec83
Sunit Roy <iamsunitroy03@gmail.com>
2022-01-03 23:32:34 +0530

    Logic for calculating days left

--- app/src/main/java/org/openlake/sampoorna/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/org/openlake/sampoorna/ui/fragments/TrackingFragment.kt
@@ -10,33 +10,22 @@ import android.view.ViewGroup
 import android.widget.DatePicker
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import org.openlake.sampoorna.databinding.FragmentTrackingBinding
 import java.util.*
 
 
-import android.widget.LinearLayout
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.transition.MaterialFadeThrough
 
 
-class TrackingFragment : Fragment(R.layout.fragment_tracking), OnDateSetListener,
-    View.OnClickListener {
-    private var dateText: TextView? = null
+class TrackingFragment : Fragment(R.layout.fragment_tracking), OnDateSetListener {
+    lateinit var dateText: TextView
+    private val menstrualCycle: Int = 28
 
 
-    private fun showDatePickerDialog() {
-
-        val datePickerDialog = this.context?.let {
-            DatePickerDialog(
-                it,
-                this,
-                Calendar.getInstance()[Calendar.YEAR],
-                Calendar.getInstance()[Calendar.MONTH],
-                Calendar.getInstance()[Calendar.DAY_OF_MONTH]
-            )
-        }
-        if (datePickerDialog != null) {
-            datePickerDialog.show()
-        }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        exitTransition = MaterialFadeThrough()
+        enterTransition = MaterialFadeThrough()
     }
 
     override fun onCreateView(
@@ -53,67 +42,52 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking), OnDateSetListener
 
 
         val button = cl.findViewById<View>(R.id.show_dialog)
-        button?.setOnClickListener(this)
+        button?.setOnClickListener { showDatePickerDialog() }
 
         return cl
     }
 
 
-    override fun onDateSet(view: DatePicker, year: Int, month: Int, dayOfMonth: Int) {
-        val calendar = Calendar.getInstance()
-        val nowDate = calendar.get(Calendar.DATE)
-        val nowMonth = calendar.get(Calendar.MONTH)+1;
-        var temp = month+1
-        var daysleft = 0
-        var f = 0
+    override fun onDateSet(view: DatePicker, year: Int, month: Int, date: Int) {
 
+        // Current Time and Date
+        val calendar: Calendar = Calendar.getInstance()
+        val today: Date = calendar.time
 
-        var finaldate = dayOfMonth + 28
-        if (month == 2 || month == 4 || month == 6 || month == 9 || month == 11) {
-            if (finaldate > 30) {
-                finaldate = finaldate - 30
+        // Selected Time
+        val selectedTime: Calendar = Calendar.getInstance()
+        selectedTime.set(year, month, date)
 
-            }
-        }
-        else {
-            if (finaldate > 31) {
-                finaldate = finaldate - 31
+        // adding menstrualCycle to selected time
+        selectedTime.add(Calendar.DATE, menstrualCycle)
 
-            }
-        }
+        // Expected future date
+        val futureDate = selectedTime.time
 
+        // Calculating left days
+        val timeLeft = futureDate.time - today.time
+        val secondsLeft = timeLeft / 1000
+        val minutesLeft = secondsLeft / 60
+        val hoursLeft = minutesLeft / 60
+        val daysLeft = hoursLeft / 24
 
-
-          if(nowMonth==1 && temp==12 && finaldate>=nowDate) {
-              System.out.println("heyy")
-              f = 1;
-              daysleft = finaldate - nowDate
-          }
-          else if(nowMonth==temp &&  dayOfMonth<=nowDate){
-              f = 1;
-              daysleft = finaldate - nowDate
-          }
-
-          else if(temp<nowMonth && finaldate>=nowDate){
-              f = 1;
-              daysleft = finaldate - nowDate
-          }
-
-
-
-
-
-            if(f==1)
-
-            this.dateText!!.text = daysleft.toString()
-            else 
-
-                this.dateText!!.text = "Invalid"
+        if (timeLeft > 0)
+            this.dateText!!.text = daysLeft.toString()
+        else
+            this.dateText!!.text = "Invalid Date"
 
     }
 
-    override fun onClick(p0: View?) {
+    private fun showDatePickerDialog() {
 
-        showDatePickerDialog()
+        context?.let {
+            DatePickerDialog(
+                it,
+                this,
+                Calendar.getInstance()[Calendar.YEAR],
+                Calendar.getInstance()[Calendar.MONTH],
+                Calendar.getInstance()[Calendar.DAY_OF_MONTH]
+            )
+        }?.show()
     }
 }
