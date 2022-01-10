25b8be34237a0ff3a714ac39082482101aa94c3c
Sunit Roy <iamsunitroy03@gmail.com>
2022-01-10 23:34:53 +0530

    Update TrackingFragment.kt

--- app/src/main/java/org/openlake/sampoorna/presentation/features/periodtracker/TrackingFragment.kt
+++ app/src/main/java/org/openlake/sampoorna/presentation/features/periodtracker/TrackingFragment.kt
@@ -5,6 +5,7 @@ import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
 import android.content.Context
 import android.content.SharedPreferences
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,23 +13,12 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.DatePicker
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.google.android.material.transition.MaterialFadeThrough
 import org.openlake.sampoorna.R
 import org.openlake.sampoorna.databinding.FragmentTrackingBinding
 import java.util.*
-import android.graphics.drawable.AnimationDrawable
-
-
-
-import androidx.constraintlayout.widget.ConstraintLayout
-
-
-
-
-
-
-
 
 
 class TrackingFragment : Fragment(R.layout.fragment_tracking), OnDateSetListener {
@@ -36,6 +26,7 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking), OnDateSetListener
     private var _binding: FragmentTrackingBinding? = null
     private val binding get() = _binding!!
     private lateinit var dateText: TextView
+    private lateinit var sharedPref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,13 +39,23 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking), OnDateSetListener
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        _binding = FragmentTrackingBinding.inflate(inflater,container,false)
+        _binding = FragmentTrackingBinding.inflate(inflater, container, false)
 
-
+        sharedPref = requireActivity().getPreferences(Context.MODE_PRIVATE)
         dateText = binding.dateText
+
 
         val button: Button = binding.showDialog
         button.setOnClickListener { showDatePickerDialog() }
+
+        if (sharedPref.contains("futureDate")) {
+            val futureDate = sharedPref.getLong("futureDate", 0)
+            // Calculating left days
+            val daysLeft = calculateDaysLeft(futureDate)
+            showDaysLeft(daysLeft)
+        } else {
+            dateText.text = "Please enter the last date of /n the menstrual period"
+        }
 
         //adding animation
         val constraintLayout: ConstraintLayout = binding.layout
@@ -74,10 +75,6 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking), OnDateSetListener
 
     override fun onDateSet(view: DatePicker, year: Int, month: Int, date: Int) {
 
-        // Current Time and Date
-        val calendar: Calendar = Calendar.getInstance()
-        val today: Date = calendar.time
-
         // Selected Time
         val selectedTime: Calendar = Calendar.getInstance()
         selectedTime.set(year, month, date)
@@ -89,27 +86,38 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking), OnDateSetListener
         val futureDate = selectedTime.time
 
         // Calculating left days
-        val timeLeft = futureDate.time - today.time
+        val daysLeft = calculateDaysLeft(futureDate.time)
+
+        //showing the results
+        showDaysLeft(daysLeft)
+
+        //saving the result
+        sharedPref.edit()?.putLong("futureDate", futureDate.time)?.apply()
+    }
+
+    private fun showDaysLeft(daysLeft: Long) {
+
+        if (daysLeft > 0)
+            dateText.text = "$daysLeft Days Left";
+        else
+            this.dateText.text = "Please enter a valid Date"
+
+    }
+
+    private fun calculateDaysLeft(futureDate: Long): Long {
+
+        // Current Time and Date
+        val calendar: Calendar = Calendar.getInstance()
+        val today: Date = calendar.time
+
+        // Calculating left days
+        val timeLeft = futureDate - today.time
         val secondsLeft = timeLeft / 1000
         val minutesLeft = secondsLeft / 60
         val hoursLeft = minutesLeft / 60
         val daysLeft = hoursLeft / 24
 
-        //saving the result
-
-        var sharedPref : SharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
-
-        sharedPref?.edit()?.putString("bla", daysLeft.toString())?.apply()
-
-        val result=sharedPref.getString("bla", "")
-
-
-        if (timeLeft > 0)
-            dateText.setText(result);
-            //this.dateText.text = sharedPref.toString()
-        else
-            this.dateText.text = "Invalid Date"
-
+        return daysLeft
     }
 
     private fun showDatePickerDialog() {
