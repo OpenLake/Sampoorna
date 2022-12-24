77e26a91252a0101e0312f541870ba16389460b5
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-12-24 17:22:19 +0530

    Created blog entity, working on ui

--- app/src/main/java/org/openlake/sampoorna/presentation/features/blogs/CreateBlogFragment.kt
+++ app/src/main/java/org/openlake/sampoorna/presentation/features/blogs/CreateBlogFragment.kt
@@ -5,19 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.NestedScrollView
+import androidx.lifecycle.ViewModelProvider
 import org.openlake.sampoorna.databinding.FragmentCreateBlogBinding
 
 class CreateBlogFragment : Fragment() {
 
     private var _binding: FragmentCreateBlogBinding? = null
     private val binding: FragmentCreateBlogBinding get() = _binding!!
+    private lateinit var blogViewModel: BlogViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentCreateBlogBinding.inflate(inflater, container, false)
+
+        blogViewModel = ViewModelProvider(this)[BlogViewModel::class.java]
+
+        binding.createBlogScroll.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+            if(scrollY>oldScrollY) {
+                binding.postBlogButton.shrink()
+            } else {
+                binding.postBlogButton.extend()
+            }
+        })
 
         return binding.root
     }
